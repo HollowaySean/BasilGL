07e4f9f985132705732ec4ab015758deb7218d8d
HollowaySean <hollowayseanm@gmail.com>
2024-03-05 23:16:52 -0800

    More ProcessController test coverage

--- tst/Process/ProcessController.test.cpp
+++ tst/Process/ProcessController.test.cpp
@@ -44,3 +44,76 @@ TEST_CASE("Process_ProcessController_addProcess") {
             == ProcessPrivilege::NONE);
     }
 }
+
+void testControllerState(
+        ProcessController controller,
+        std::shared_ptr<ProcessInstance> instance,
+        ProcessPrivilege privilege,
+        ProcessState state,
+        ProcessControllerState expected) {
+    instance->privilegeLevel = privilege;
+    instance->process->setCurrentState(state);
+
+    controller.interpretProcessState(instance);
+    REQUIRE(controller.getCurrentState() == expected);
+}
+
+TEST_CASE("Process_ProcessController_interpretProcessState") {
+    ProcessController controller = ProcessController();
+    std::shared_ptr<IProcess> process = std::make_shared<TestProcess>();
+    auto instance = std::make_shared<ProcessInstance>(process);
+
+    SECTION("Kills controller if requested with high permission") {
+        testControllerState(controller, instance,
+            ProcessPrivilege::HIGH,
+            ProcessState::REQUEST_KILL,
+            ProcessControllerState::KILLED);
+    }
+
+    SECTION("Stops controller if requested kill with low permission") {
+        testControllerState(controller, instance,
+            ProcessPrivilege::LOW,
+            ProcessState::REQUEST_KILL,
+            ProcessControllerState::STOPPING);
+    }
+
+    SECTION("Does nothing if requested kill with no permission") {
+        testControllerState(controller, instance,
+            ProcessPrivilege::NONE,
+            ProcessState::REQUEST_KILL,
+            ProcessControllerState::READY);
+    }
+
+    SECTION("Stops if requested stop with high permission") {
+        testControllerState(controller, instance,
+            ProcessPrivilege::HIGH,
+            ProcessState::REQUEST_STOP,
+            ProcessControllerState::STOPPING);
+    }
+
+    SECTION("Stops if requested stop with low permission") {
+        testControllerState(controller, instance,
+            ProcessPrivilege::LOW,
+            ProcessState::REQUEST_STOP,
+            ProcessControllerState::STOPPING);
+    }
+
+    SECTION("Does nothing if requested stop with no permission") {
+        testControllerState(controller, instance,
+            ProcessPrivilege::NONE,
+            ProcessState::REQUEST_STOP,
+            ProcessControllerState::READY);
+    }
+
+    SECTION("Removes process if requested") {
+        ProcessSchedule& schedule = controller.schedule;
+
+        schedule.addProcess(instance);
+        REQUIRE(schedule.size() == 1);
+
+        instance->process->setCurrentState(ProcessState::REMOVE_PROCESS);
+        controller.interpretProcessState(instance);
+
+        REQUIRE(schedule.size() == 0);
+    }
+}
