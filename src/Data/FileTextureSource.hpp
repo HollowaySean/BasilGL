4d382eefa8f357b935a8df7ce1bbb10ce0f07181
Sean Holloway <hollowayseanm@gmail.com>
2024-03-11 20:55:42 -0500

    Add stb library as submodule

---
+++ src/Data/FileTextureSource.hpp
@@ -0,0 +1,15 @@
+#ifndef SRC_DATA_FILETEXTURESOURCE_HPP_
+#define SRC_DATA_FILETEXTURESOURCE_HPP_
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
+
+namespace basil {
+
+class FileTextureSource : public ITextureSource {
+ public:
+};
+
+};
+
+#endif  // SRC_DATA_FILETEXTURESOURCE_HPP_
