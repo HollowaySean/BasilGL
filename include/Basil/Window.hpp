e6ec663eb0216e068a3fa2dfd82557b77395d821
Sean Holloway <hollowayseanm@gmail.com>
2024-04-16 22:00:43 -0700

    Minor cleanup (#45)

--- include/Basil/Window.hpp
+++ include/Basil/Window.hpp
@@ -2,9 +2,9 @@
 #define INCLUDE_BASIL_WINDOW_HPP_
 
 #include "Window/GLShader.hpp"
+#include "Window/GLShaderPane.hpp"
 #include "Window/GLShaderProgram.hpp"
 #include "Window/GLTexture.hpp"
-#include "Window/GLShaderPane.hpp"
 #include "Window/IPane.hpp"
 #include "Window/SplitPane.hpp"
 #include "Window/WindowView.hpp"
