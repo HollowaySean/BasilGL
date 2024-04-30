1b837740a72ad634b277fef59d876269f294fe10
Sean Holloway <hollowayseanm@gmail.com>
2024-04-30 14:56:11 -0700

    Create LambdaProcess utility (#53)
    Resolves #40

---
+++ tst/Utility/LambdaProcess.test.cpp
@@ -0,0 +1,17 @@
+#include <catch.hpp>
+
+#include <Basil/Utility.hpp>
+
+using basil::LambdaProcess;
+
+TEST_CASE("Utility_LambdaProcess_onLoop") {
+    bool hasRun = false;
+    std::function<void()> lambda = [&]() { hasRun = true; };
+
+    SECTION("Runs a given lambda during main loop") {
+        auto process = LambdaProcess(lambda);
+        process.onLoop();
+
+        REQUIRE(hasRun);
+    }
+}
