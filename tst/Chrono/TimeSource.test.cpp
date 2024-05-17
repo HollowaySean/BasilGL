a9179bf7729df6c869aa71a88af80a585858e7b7
HollowaySean <hollowayseanm@gmail.com>
2024-05-17 08:46:17 -0700

    Corrected test case prefixes

--- tst/Chrono/TimeSource.test.cpp
+++ tst/Chrono/TimeSource.test.cpp
@@ -10,7 +10,7 @@ using basil::TimeSource;
 using TSTest = TimeSource<TestClock>;
 using TSChrono = TimeSource<std::chrono::steady_clock>;
 
-TEST_CASE("Process_TimeSource_getTimestamp") {
+TEST_CASE("Chrono_TimeSource_getTimestamp") {
     SECTION("Returns value specified by clock source") {
         TestClock::setNextTimeStamp(0);
         CHECK(TSTest::getTimestamp() ==
@@ -22,7 +22,7 @@ TEST_CASE("Process_TimeSource_getTimestamp") {
     }
 }
 
-TEST_CASE("Process_TimeSource_waitForDuration") {
+TEST_CASE("Chrono_TimeSource_waitForDuration") {
     SECTION("Sleeps for time provided") {
         auto sleepTime = std::chrono::milliseconds(100);
 
@@ -34,7 +34,7 @@ TEST_CASE("Process_TimeSource_waitForDuration") {
     }
 }
 
-TEST_CASE("Process_TimeSource_waitUntilTime") {
+TEST_CASE("Chrono_TimeSource_waitUntilTime") {
     SECTION("Sleeps for time provided") {
         auto sleepTime = std::chrono::milliseconds(100);
 
@@ -46,7 +46,7 @@ TEST_CASE("Process_TimeSource_waitUntilTime") {
     }
 }
 
-TEST_CASE("Process_TimeSource_frequencyToPeriod") {
+TEST_CASE("Chrono_TimeSource_frequencyToPeriod") {
     SECTION("Returns zero for zero input") {
         auto period = TSTest::frequencyToPeriod(0);
         CHECK(period == std::chrono::seconds(0));
@@ -58,7 +58,7 @@ TEST_CASE("Process_TimeSource_frequencyToPeriod") {
     }
 }
 
-TEST_CASE("Process_TimeSource_periodToFrequency") {
+TEST_CASE("Chrono_TimeSource_periodToFrequency") {
     SECTION("Returns zero for zero input") {
         auto period = std::chrono::milliseconds(0);
         double frequency = TSTest::periodToFrequency(period);
