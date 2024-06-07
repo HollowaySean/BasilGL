8f3e258fb3d25dbec93d497cf800cfbc27e0c3f4
Sean Holloway <hollowayseanm@gmail.com>
2024-06-06 23:25:14 -0500

    Added TODOs

--- examples/raytracer/main.cpp
+++ examples/raytracer/main.cpp
@@ -14,6 +14,16 @@ int main(int argc, char** argv) {
 
     basil::Logger::get().setLevel(basil::LogLevel::DEBUG);
 
+    // TODO(sholloway): Keyboard camera movement
+    // TODO(sholloway): Mouse camera rotation
+    // TODO(sholloway): Parameterized spheres
+    // TODO(sholloway): More advanced rendering
+    // TODO(sholloway): Cubemap loading from JSON
+    // TODO(sholloway): Vector loading from JSON
+    // TODO(sholloway): Move files to src folder
+    // TODO(sholloway): Stats and controls
+    // TODO(sholloway): Screenshot tool
+
     auto basilApp = basil::BasilApp::Builder()
         .withController(basil::ProcessController::Builder()
             .build())
