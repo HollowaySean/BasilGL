7cb13ddb7a77c0d78a52f7a3eb4159ea2ed4db3a
Sean Holloway <hollowayseanm@gmail.com>
2025-08-30 23:03:25 -0700

    Re-enable BasilContext tests

--- tst/Context/BasilContext.test.cpp
+++ tst/Context/BasilContext.test.cpp
@@ -7,128 +7,127 @@ using basil::BasilContext;
 using basil::Logger;
 using basil::LogLevel;
 
-// TEST_CASE("Context_BasilContext_setGLFW[...]Callback") { BASIL_LOCK_TEST
-//     bool hasBeenCalled = false;
-//     auto callback =
-//         [&](int...){ hasBeenCalled = true; };
+TEST_CASE("Context_BasilContext_setGLFW[...]Callback") { BASIL_LOCK_TEST
+    bool hasBeenCalled = false;
+    auto callback =
+        [&](int...){ hasBeenCalled = true; };
 
-//     SECTION("setGLFWFramebufferSizeCallback") {
-//         unsigned int ID =
-//             BasilContext::setGLFWFramebufferSizeCallback(callback);
-//         CHECK(BasilContext::framebufferCallbacks.contains(ID));
+    BasilContext& context = BasilContext::get();
 
-//         CHECK_FALSE(hasBeenCalled);
-//         BasilContext::onFrameBufferResize(nullptr, 0, 0);
-//         CHECK(hasBeenCalled);
+    SECTION("setGLFWFramebufferSizeCallback") {
+        unsigned int ID =
+            context.setGLFWFramebufferSizeCallback(callback);
+        CHECK(context.framebufferCallbacks.contains(ID));
 
-//         BasilContext::removeGLFWFramebufferSizeCallback(ID);
-//         CHECK_FALSE(BasilContext::framebufferCallbacks.contains(ID));
-//     }
+        CHECK_FALSE(hasBeenCalled);
+        context.onFrameBufferResize(nullptr, 0, 0);
+        CHECK(hasBeenCalled);
 
-//     SECTION("setGLFWMouseButtonCallback") {
-//         unsigned int ID =
-//             BasilContext::setGLFWMouseButtonCallback(callback);
-//         CHECK(BasilContext::mouseButtonCallbacks.contains(ID));
+        context.removeGLFWFramebufferSizeCallback(ID);
+        CHECK_FALSE(context.framebufferCallbacks.contains(ID));
+    }
 
-//         CHECK_FALSE(hasBeenCalled);
-//         BasilContext::onMouseButton(nullptr, 0, 0, 0);
-//         CHECK(hasBeenCalled);
+    SECTION("setGLFWMouseButtonCallback") {
+        unsigned int ID =
+            context.setGLFWMouseButtonCallback(callback);
+        CHECK(context.mouseButtonCallbacks.contains(ID));
 
-//         BasilContext::removeGLFWMouseButtonCallback(ID);
-//         CHECK_FALSE(BasilContext::mouseButtonCallbacks.contains(ID));
-//     }
+        CHECK_FALSE(hasBeenCalled);
+        context.onMouseButton(nullptr, 0, 0, 0);
+        CHECK(hasBeenCalled);
 
-//     SECTION("setGLFWKeyCallback") {
-//         unsigned int ID =
-//             BasilContext::setGLFWKeyCallback(callback);
-//         CHECK(BasilContext::keyCallbacks.contains(ID));
+        context.removeGLFWMouseButtonCallback(ID);
+        CHECK_FALSE(context.mouseButtonCallbacks.contains(ID));
+    }
 
-//         BasilContext::setGLFWKeyCallback(callback);
-//         CHECK_FALSE(hasBeenCalled);
-//         BasilContext::onKeyAction(nullptr, 0, 0, 0, 0);
-//         CHECK(hasBeenCalled);
+    SECTION("setGLFWKeyCallback") {
+        unsigned int ID =
+            context.setGLFWKeyCallback(callback);
+        CHECK(context.keyCallbacks.contains(ID));
 
-//         BasilContext::removeGLFWKeyCallback(ID);
-//         CHECK_FALSE(BasilContext::keyCallbacks.contains(ID));
-//     }
+        context.setGLFWKeyCallback(callback);
+        CHECK_FALSE(hasBeenCalled);
+        context.onKeyAction(nullptr, 0, 0, 0, 0);
+        CHECK(hasBeenCalled);
 
-//     SECTION("setGLFWCursorEnterCallback") {
-//         unsigned int ID =
-//             BasilContext::setGLFWCursorEnterCallback(callback);
-//         CHECK(BasilContext::cursorEnterCallbacks.contains(ID));
+        context.removeGLFWKeyCallback(ID);
+        CHECK_FALSE(context.keyCallbacks.contains(ID));
+    }
 
-//         BasilContext::setGLFWCursorEnterCallback(callback);
-//         CHECK_FALSE(hasBeenCalled);
-//         BasilContext::onCursorEnter(nullptr, 0);
-//         CHECK(hasBeenCalled);
+    SECTION("setGLFWCursorEnterCallback") {
+        unsigned int ID =
+            context.setGLFWCursorEnterCallback(callback);
+        CHECK(context.cursorEnterCallbacks.contains(ID));
 
-//         BasilContext::removeGLFWCursorEnterCallback(ID);
-//         CHECK_FALSE(BasilContext::cursorEnterCallbacks.contains(ID));
-//     }
-// }
+        context.setGLFWCursorEnterCallback(callback);
+        CHECK_FALSE(hasBeenCalled);
+        context.onCursorEnter(nullptr, 0);
+        CHECK(hasBeenCalled);
 
-// TEST_CASE("Context_BasilContext_logGLFWError") { BASIL_LOCK_TEST
-//     Logger& logger = Logger::get();
+        context.removeGLFWCursorEnterCallback(ID);
+        CHECK_FALSE(context.cursorEnterCallbacks.contains(ID));
+    }
+}
 
-//     SECTION("Logs info on success") {
-//         BasilContext::logGLFWError(1);
+TEST_CASE("Context_BasilContext_logGLFWError") { BASIL_LOCK_TEST
+    Logger& logger = Logger::get();
 
-//         CHECK(logger.getLastOutput() ==
-//             "GLFW context initialized successfully.");
-//         CHECK(logger.getLastLevel() ==
-//             LogLevel::INFO);
-//     }
+    SECTION("Logs info on success") {
+        BasilContext::logGLFWError(1);
 
-//     SECTION("Logs error on failure") {
-//         BasilContext::logGLFWError(0);
+        CHECK(logger.getLastOutput() ==
+            "GLFW context initialized successfully.");
+        CHECK(logger.getLastLevel() ==
+            LogLevel::INFO);
+    }
 
-//         CHECK(logger.getLastLevel() == LogLevel::ERROR);
-//     }
-// }
+    SECTION("Logs error on failure") {
+        BasilContext::logGLFWError(0);
 
-// TEST_CASE("Context_BasilContext_logGLFWWindowError") { BASIL_LOCK_TEST
-//     Logger& logger = Logger::get();
+        CHECK(logger.getLastLevel() == LogLevel::ERROR);
+    }
+}
 
-//     SECTION("Logs info on success") {
-//         GLFWwindow* window = BasilContext::getGLFWWindow();
-//         BasilContext::logGLFWWindowError(window);
+TEST_CASE("Context_BasilContext_logGLFWWindowError") { BASIL_LOCK_TEST
+    Logger& logger = Logger::get();
+    BasilContext& context = BasilContext::get();
 
-//         CHECK(logger.getLastOutput() ==
-//             "GLFW window created successfully.");
-//         CHECK(logger.getLastLevel() ==
-//             LogLevel::INFO);
-//     }
+    SECTION("Logs info on success") {
+        GLFWwindow* window = context.getGLFWWindow();
+        BasilContext::logGLFWWindowError(window);
 
-//     SECTION("Logs error and terminates on failure") {
-//         BasilContext::initialize();
-//         CHECK(BasilContext::isInitialized());
+        CHECK(logger.getLastOutput() ==
+            "GLFW window created successfully.");
+        CHECK(logger.getLastLevel() ==
+            LogLevel::INFO);
+    }
 
-//         BasilContext::logGLFWWindowError(nullptr);
+    SECTION("Logs error on failure") {
+        context.initialize();
+        CHECK(context.isInitialized());
 
-//         CHECK(logger.getLastOutput() == "GLFW failed to create window.");
-//         CHECK(logger.getLastLevel() == LogLevel::ERROR);
-//         CHECK_FALSE(BasilContext::isInitialized());
+        BasilContext::logGLFWWindowError(nullptr);
 
-//         // Re-initialize to prevent issues
-//         BasilContext::initialize();
-//     }
-// }
+        CHECK(logger.getLastOutput() == "GLFW failed to create window.");
+        CHECK(logger.getLastLevel() == LogLevel::ERROR);
+    }
+}
 
-// TEST_CASE("Context_BasilContext_logGLEWError") { BASIL_LOCK_TEST
-//     Logger& logger = Logger::get();
+TEST_CASE("Context_BasilContext_logGLEWError") { BASIL_LOCK_TEST
+    Logger& logger = Logger::get();
 
-//     SECTION("Logs info on success") {
-//         BasilContext::logGLEWError(GLEW_OK);
+    SECTION("Logs info on success") {
+        BasilContext::logGLEWError(GLEW_OK);
 
-//         CHECK(logger.getLastOutput() ==
-//             "GLEW context initialized successfully.");
-//         CHECK(logger.getLastLevel() ==
-//             LogLevel::INFO);
-//     }
+        CHECK(logger.getLastOutput() ==
+            "GLEW context initialized successfully.");
+        CHECK(logger.getLastLevel() ==
+            LogLevel::INFO);
+    }
 
-//     SECTION("Logs error on failure") {
-//         BasilContext::logGLEWError(!GLEW_OK);
+    SECTION("Logs error on failure") {
+        BasilContext::logGLEWError(!GLEW_OK);
 
-//         CHECK(logger.getLastLevel() == LogLevel::ERROR);
-//     }
-// }
+        CHECK(logger.getLastLevel() == LogLevel::ERROR);
+    }
+}
